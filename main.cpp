1a018b7c58ca55568fa10dd3e9a062a81cf18783
Dan Powell <dp@danielpowell.com>
2022-09-14 18:28:10 +0000

    Testing this keyword

--- main.cpp
+++ main.cpp
@@ -1,26 +1,38 @@
-#include<iostream>
+#include <iostream>
+ 
 using namespace std;
-  
-/* local variable is same as a member's name */
-class Test
-{
-private:
-   int x;
-public:
-   void setX (int x)
-   {
-       // The 'this' pointer is used to retrieve the object's x
-       // hidden by the local variable 'x'
-       this->x = x;
-   }
-   void print() { cout << "x = " << x << endl; }
+
+class Box {
+   public:
+      // Constructor definition
+      Box(double l = 2.0, double b = 2.0, double h = 2.0) {
+         cout <<"Constructor called." << endl;
+         length = l;
+         breadth = b;
+         height = h;
+      }
+      double Volume() {
+         return length * breadth * height;
+      }
+      int compare(Box box) {
+         return this->Volume() > box.Volume();
+      }
+      
+   private:
+      double length;     // Length of a box
+      double breadth;    // Breadth of a box
+      double height;     // Height of a box
 };
-  
-int main()
-{
-   Test obj;
-   int x = 20;
-   obj.setX(x);
-   obj.print();
+
+int main(void) {
+   Box Box1(3.3, 1.2, 1.5);    // Declare box1
+   Box Box2(8.5, 6.0, 2.0);    // Declare box2
+
+   if(Box1.compare(Box2)) {
+      cout << "Box2 is smaller than Box1" <<endl;
+   } else {
+      cout << "Box2 is equal to or larger than Box1" <<endl;
+   }
+   
    return 0;
 }
